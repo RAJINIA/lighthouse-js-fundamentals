397d1a128a576c95147b74584c31c23ac51f1f8a
RAJINIA <rajini1608@gmail.com>
2021-10-01 06:22:23 -0600

    additional functions

--- hello.js
+++ hello.js
@@ -7,3 +7,22 @@ const sayHello  = function (name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John');
+
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+
+
+
+const returnSayHello1  = function (name) {
+  return "Hello, " + name;
+}
+const greeting1 = returnSayHello1('John');
+console.log(greeting1);
